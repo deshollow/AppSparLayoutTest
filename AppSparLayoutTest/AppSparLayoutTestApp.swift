ebe8b26f5773b7be28dfd82cafeba7b29cf17336
deshollow <555demin@gmail.com>
2024-02-02 13:24:08 +0300

    Initial Commit

---
+++ AppSparLayoutTest/AppSparLayoutTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSparLayoutTestApp.swift
+//  AppSparLayoutTest
+//
+//  Created by deshollow on 02.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppSparLayoutTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
